cc2eb9a2032a3cde382975832529056f8340769a
ROFL1ST <pr.endra48@gmail.com>
2025-10-27 16:17:49 +0700

    fix: cors

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(
   cors({
     origin: [
       "https://ksm-uat.mafiincloud.com",
-      "http://localhost:8080",
-      "https://hwx1l9qw-8080.asse.devtunnels.ms/",
+      // "http://localhost:8080",
+      // "https://hwx1l9qw-8080.asse.devtunnels.ms/",
       // "https://gl4wt6xl-8080.asse.devtunnels.ms",
       // "https://gl4wt6xl-8080.asse.devtunnels.ms",
     ],
